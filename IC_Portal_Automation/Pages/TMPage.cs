3521da97ce835a71a114fdfd724e6e6e5e7f1feb
lemineiro <leticia.ltm@gmail.com>
2024-03-14 14:50:29 +1300

    Created Page Object Model and Edit & Delete flows

---
+++ IC_Portal_Automation/Pages/TMPage.cs
@@ -0,0 +1,73 @@
+﻿using IC_Portal_Automation.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC_Portal_Automation.Pages
+{
+    public class TMPage
+    {
+        public void CreateTMRecord(IWebDriver driver)
+        {
+            //Create a new Time record
+            //Click on the Create New button
+            IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+            createNewButton.Click();
+            ////a[contains(text(),'Create New')]
+
+            //Select Time on TypeCode
+            IWebElement typeCodeMainDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span"));
+            typeCodeMainDropdown.Click();
+
+            Thread.Sleep(2000);
+
+            IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+            typeCodeDropdown.Click();
+
+            //Enter Code
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("CodeTest");
+
+            //Enter Description
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("DescriptionTest");
+
+            //Enter Price Unit
+            IWebElement priceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+            priceTextbox.SendKeys("10");
+
+            //Click on Save
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Wait.WaitToBeClickable(driver, "XPath", "//*[@id='tmsGrid']/div[4]/a[4]/span", 2);
+
+            //Check if the new time record has been created
+            IWebElement goToTheLastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            goToTheLastPageButton.Click();
+
+            Wait.WaitToBeVisible(driver, "XPath", "//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]", 2);
+
+            IWebElement newRecordCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+            if (newRecordCode.Text == "CodeTest")
+            {
+                Console.WriteLine("New Time record has been created successfully");
+            }
+            else
+            {
+                Console.WriteLine("Test failed");
+            }
+        }
+        public void EditTMRecord(IWebDriver driver)
+        {
+
+        }
+        public void DeleteTMRecord(IWebDriver driver)
+        {
+
+        }
+    }
+}
---
+++ IC_Portal_Automation/Tests/TM_Test.cs
@@ -0,0 +1,26 @@
+﻿using IC_Portal_Automation.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace IC_Portal_Automation.Tests
+{
+    public class TM_Test
+    {
+        private static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginActions(driver, "hari", "123123");
+
+            HomePage homePageObj = new HomePage();
+            //homePageObj.VerifyLoggedInUser(driver);
+            homePageObj.NavigateToTMPage(driver);
+
+            TMPage tMPageObj = new TMPage();
+            tMPageObj.CreateTMRecord(driver);
+            tMPageObj.EditTMRecord(driver);
+            tMPageObj.DeleteTMRecord(driver);
+        }
+    }
+}
